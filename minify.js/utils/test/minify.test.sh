7e6d11e3604dc463fef212cc9db916f67a38c9c4
adamlui@protonmail.com <adamlui@protonmail.com>
2024-02-22 07:11:32 -0800

    Re-cased vars to snake case

--- minify.js/utils/test/minify.test.sh
+++ minify.js/utils/test/minify.test.sh
@@ -6,38 +6,38 @@ br='\033[1;91m' # bright red
 bg='\033[1;92m' # bright green
 
 # Init minify.js path
-searchPath="$(realpath "$0")" # start in test script path
-while [[ ! -f "$searchPath/minify.js" ]] # minify.js not found
-    do searchPath="$(dirname "$searchPath")" # nav up one
-done ; minifyJSpath="$searchPath/minify.js"
+search_path="$(realpath "$0")" # start in test script path
+while [[ ! -f "$search_path/minify.js" ]] # minify.js not found
+    do search_path="$(dirname "$search_path")" # nav up one
+done ; minifyjs_path="$search_path/minify.js"
 
 # Init I/O args
-inputArg="utils/test/input"
-outputArg="output/min"
+input_arg="utils/test/input"
+output_arg="output/min"
 
 # Run minify command
-testCmd="node $minifyJSpath $inputArg $outputArg"
-echo -e "\n> Running '$testCmd'..."
-if ! $testCmd ; then
-    echo -e "\n${br}Error executing command: $testCmd${nc}"
+test_cmd="node $minifyjs_path $input_arg $output_arg"
+echo -e "\n> Running '$test_cmd'..."
+if ! $test_cmd ; then
+    echo -e "\n${br}Error executing command: $test_cmd${nc}"
     exit 1
 fi
 
 # Compare generated files to expected output
 echo "> Comparing generated files to expected output..."
-inputDir="$(dirname "$0")/input"
-expectedOutputDir="$(dirname "$0")/expected_output"
-file1Expected=$(< "$expectedOutputDir/$outputArg/file1.min.js")
-file2Expected=$(< "$expectedOutputDir/nested/dir/$outputArg/file2.min.js")
-file1Actual=$(< "$inputDir/$outputArg/file1.min.js")
-file2Actual=$(< "$inputDir/nested/dir/$outputArg/file2.min.js")
+input_dir="$(dirname "$0")/input"
+expected_output_dir="$(dirname "$0")/expected_output"
+file1_expected=$(< "$expected_output_dir/$output_arg/file1.min.js")
+file2_expected=$(< "$expected_output_dir/nested/dir/$output_arg/file2.min.js")
+file1_actual=$(< "$input_dir/$output_arg/file1.min.js")
+file2_actual=$(< "$input_dir/nested/dir/$output_arg/file2.min.js")
 
 # Cleanup generated files/folders
 echo "> Cleaning up generated files/folders..."
-rm -rf "$inputDir/${outputArg%%/*}" "$inputDir/nested/dir/${outputArg%%/*}"
+rm -rf "$input_dir/${output_arg%%/*}" "$input_dir/nested/dir/${output_arg%%/*}"
 
 # Print test results
-if [ "$file1Actual" = "$file1Expected" ] && [ "$file2Actual" = "$file2Expected" ]; then
+if [ "$file1_actual" = "$file1_expected" ] && [ "$file2_actual" = "$file2_expected" ]; then
     echo -e "\n${bg}Test passed: Minification/recursion succeeded!${nc}"
 else
     echo -e "\n${br}Test failed: Generated files do not match expected output.${nc}"
