76ba66133ee74976e33c59130dc8f5f4235fb54b
adamlui@protonmail.com <adamlui@protonmail.com>
2024-03-09 09:37:57 -0800

    Add conditional function export if script not run from CLI

--- minify.js/minify.js
+++ minify.js/minify.js
@@ -65,134 +65,140 @@ function minify(input, options = { recursive: true, verbose: true }) {
     }
 }
 
-// Init UI colors
-const nc = '\x1b[0m', // no color
-      br = '\x1b[1;91m', // bright red
-      by = '\x1b[1;33m', // bright yellow
-      bg = '\x1b[1;92m'; // bright green
+// EXPORT functions if script was required
+if (require.main !== module) module.exports = { minify, findUnminnedJSfiles };
 
-// Show HELP screen if -h or --help passed
-if (process.argv.some(arg => /^--?h(?:elp)?$/.test(arg))) {
+else { // run as CLI tool
 
-    printHelp(`\n${by}minify-js [inputPath] [outputPath] [options]${nc}`);
-    printHelp('\nPath arguments:');
-    printHelp(' [inputPath]                 '
-        + 'Path to JS file or directory containing JS files to be minified,'
-        + ' relative to the current working directory.');
-    printHelp(' [outputPath]                '
-        + 'Path to file or directory where minified files will be stored,'
-        + ' relative to original file location (if not provided, min/ is used).');
-    printHelp('\nConfig options:');
-    printHelp(' -n, --dry-run               Don\'t actually minify the file(s),'
-        + ' just show if they will be processed.');
-    printHelp(' -d, --include-dotfolders    Include dotfolders in file search.');
-    printHelp(' -D, --include-dotfiles      Include dotfiles in file search.');
-    printHelp(' -q, --quiet                 Suppress all logging except errors.');
-    printHelp('\nInfo commands:');
-    printHelp(' -h, --help                  Display this help screen.');
-    printHelp(' -v, --version               Show version number.');
+    // Init UI colors
+    const nc = '\x1b[0m', // no color
+          br = '\x1b[1;91m', // bright red
+          by = '\x1b[1;33m', // bright yellow
+          bg = '\x1b[1;92m'; // bright green
 
-// Show VERSION number if -v or --version passed
-} else if (process.argv.some(arg => /^--?ve?r?s?i?o?n?$/.test(arg))) {
-    console.info('v' + require('./package.json').version);
+    // Show HELP screen if -h or --help passed
+    if (process.argv.some(arg => /^--?h(?:elp)?$/.test(arg))) {
 
-} else { // run MAIN routine
+        printHelp(`\n${by}minify-js [inputPath] [outputPath] [options]${nc}`);
+        printHelp('\nPath arguments:');
+        printHelp(' [inputPath]                 '
+            + 'Path to JS file or directory containing JS files to be minified,'
+            + ' relative to the current working directory.');
+        printHelp(' [outputPath]                '
+            + 'Path to file or directory where minified files will be stored,'
+            + ' relative to original file location (if not provided, min/ is used).');
+        printHelp('\nConfig options:');
+        printHelp(' -n, --dry-run               Don\'t actually minify the file(s),'
+            + ' just show if they will be processed.');
+        printHelp(' -d, --include-dotfolders    Include dotfolders in file search.');
+        printHelp(' -D, --include-dotfiles      Include dotfiles in file search.');
+        printHelp(' -q, --quiet                 Suppress all logging except errors.');
+        printHelp('\nInfo commands:');
+        printHelp(' -h, --help                  Display this help screen.');
+        printHelp(' -v, --version               Show version number.');
 
-    // Init I/O args
-    const [inputArg = '', outputArg = ''] = ( // default to empty strings for error-less handling
-        process.argv.slice(2) // exclude executable and script paths
-            .filter(arg => !arg.startsWith('-')) // exclude flags
-            .map(arg => arg.replace(/^\/*/, '')) // clean leading slashes to avoid parsing system root
-    );
+    // Show VERSION number if -v or --version passed
+    } else if (process.argv.some(arg => /^--?ve?r?s?i?o?n?$/.test(arg))) {
+        console.info('v' + require('./package.json').version);
 
-    // Validate input arg (output arg can be anything)
-    const inputPath = path.resolve(process.cwd(), inputArg);
-    if (inputArg && !fs.existsSync(inputPath)) {
-        console.error(`\n${br}Error: First arg must be an existing file or directory.`
-            + `\n${ inputPath } does not exist.${nc}`
-            + `\n\n${bg}Example valid command: \n>> minify-js . output.min.js${nc}`
-            + `\n\n${by}For all command options: \n>> minify-js --help${nc}`);
-        process.exit(1);
+    } else { // run MAIN routine
+
+        // Init I/O args
+        const [inputArg = '', outputArg = ''] = ( // default to empty strings for error-less handling
+            process.argv.slice(2) // exclude executable and script paths
+                .filter(arg => !arg.startsWith('-')) // exclude flags
+                .map(arg => arg.replace(/^\/*/, '')) // clean leading slashes to avoid parsing system root
+        );
+
+        // Validate input arg (output arg can be anything)
+        const inputPath = path.resolve(process.cwd(), inputArg);
+        if (inputArg && !fs.existsSync(inputPath)) {
+            console.error(`\n${br}Error: First arg must be an existing file or directory.`
+                + `\n${ inputPath } does not exist.${nc}`
+                + `\n\n${bg}Example valid command: \n>> minify-js . output.min.js${nc}`
+                + `\n\n${by}For all command options: \n>> minify-js --help${nc}`);
+            process.exit(1);
+        }
+
+        // Recursively find all eligible JavaScript files or arg-passed file
+        const unminnedJSfiles = inputArg.endsWith('.js') ? [inputPath] : findUnminnedJSfiles(inputPath);
+
+        if (unminnedJSfiles.length === 0) { // print nothing found
+            printIfNotQuiet(`\n${by}No unminified JavaScript files found.${nc}`);
+
+        } else if (config.dryRun) { // print files to be processed
+            console.info(`\n${by}JS files to be minified:${nc}`);
+            unminnedJSfiles.forEach(file => console.info(file));
+
+        } else { // actually minify JavaScript files
+            printIfNotQuiet(''); // line break before first log
+
+            // Build array of minified code
+            const failedJSpaths = [];
+            const minifiedJSdata = unminnedJSfiles.map(jsPath => {
+                const minifiedResult = minify(jsPath);
+                if (minifiedResult.code === undefined) failedJSpaths.push(jsPath);
+                printIfNotQuiet(`Minifying ${ jsPath }...`
+                    + ( minifiedResult.code === undefined ? `${br} FAILED${nc}` : '' ));
+                return minifiedResult;
+            }).filter(file => file.code !== undefined); // filter out failed minifications
+
+            // Write array data to files
+            minifiedJSdata.forEach(({ code, srcPath }) => {
+                const outputDir = path.join(
+                    path.dirname(srcPath), // path of file to be minified
+                    /so?u?rce?$/.test(path.dirname(srcPath)) ? '../min' // + ../min/ if in *(src|source)/
+                        : outputArg.endsWith('.js') ? path.dirname(outputArg) // or path from file output arg
+                        : outputArg || 'min' // or path from folder output arg or min/ if no output arg passed
+                );
+                const outputFilename = (
+                    outputArg.endsWith('.js') && inputArg.endsWith('.js')
+                        ? path.basename(outputArg).replace(/(\.min)?\.js$/, '')
+                        : path.basename(srcPath, '.js')
+                ) + '.min.js';
+                const outputPath = path.join(outputDir, outputFilename);
+                if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true });
+                fs.writeFileSync(outputPath, code, 'utf8');
+            });
+
+            // Print final summary
+            if (minifiedJSdata.length > 0) {
+                printIfNotQuiet(`\n${bg}Minification complete!${nc}`);
+                printIfNotQuiet(
+                    `${ minifiedJSdata.length } file${ minifiedJSdata.length > 1 ? 's' : '' } minified.`);
+            } else printIfNotQuiet(`${by}No unminified JavaScript files processed successfully.${nc}`);
+            const failedCnt = unminnedJSfiles.length - minifiedJSdata.length;
+            if (failedCnt > 0) {
+                printIfNotQuiet(`\n${br + failedCnt} file${ failedCnt > 1 ? 's' : '' } failed to minify:${nc}`);
+                printIfNotQuiet(failedJSpaths.join(', '));
+            }
+        }
     }
 
-    // Recursively find all eligible JavaScript files or arg-passed file
-    const unminnedJSfiles = inputArg.endsWith('.js') ? [inputPath] : findUnminnedJSfiles(inputPath);
+    // Define LOGGING functions
 
-    if (unminnedJSfiles.length === 0) { // print nothing found
-        printIfNotQuiet(`\n${by}No unminified JavaScript files found.${nc}`);
+    function printHelp(msg) { // wrap msg + indent 2nd+ lines (for --help screen)
+        const terminalWidth = process.stdout.columns || 80,
+              indentation = 29, lines = [], words = msg.match(/\S+|\s+/g);
 
-    } else if (config.dryRun) { // print files to be processed
-        console.info(`\n${by}JS files to be minified:${nc}`);
-        unminnedJSfiles.forEach(file => console.info(file));
-
-    } else { // actually minify JavaScript files
-        printIfNotQuiet(''); // line break before first log
-
-        // Build array of minified code
-        const failedJSpaths = [];
-        const minifiedJSdata = unminnedJSfiles.map(jsPath => {
-            const minifiedResult = minify(jsPath);
-            if (minifiedResult.code === undefined) failedJSpaths.push(jsPath);
-            printIfNotQuiet(`Minifying ${ jsPath }...`
-                + ( minifiedResult.code === undefined ? `${br} FAILED${nc}` : '' ));
-            return minifiedResult;
-        }).filter(file => file.code !== undefined); // filter out failed minifications
-
-        // Write array data to files
-        minifiedJSdata.forEach(({ code, srcPath }) => {
-            const outputDir = path.join(
-                path.dirname(srcPath), // path of file to be minified
-                /so?u?rce?$/.test(path.dirname(srcPath)) ? '../min' // + ../min/ if in *(src|source)/
-                    : outputArg.endsWith('.js') ? path.dirname(outputArg) // or path from file output arg
-                    : outputArg || 'min' // or path from folder output arg or min/ if no output arg passed
-            );
-            const outputFilename = (
-                outputArg.endsWith('.js') && inputArg.endsWith('.js')
-                    ? path.basename(outputArg).replace(/(\.min)?\.js$/, '')
-                    : path.basename(srcPath, '.js')
-            ) + '.min.js';
-            const outputPath = path.join(outputDir, outputFilename);
-            if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true });
-            fs.writeFileSync(outputPath, code, 'utf8');
+        // Split msg into lines of appropriate lengths
+        let currentLine = '';
+        words.forEach(word => {
+            const lineLength = terminalWidth - ( lines.length === 0 ? 0 : indentation );
+            if (currentLine.length + word.length > lineLength) { // cap/store it
+                lines.push(lines.length === 0 ? currentLine : currentLine.trimStart());
+                currentLine = '';
+            }
+            currentLine += word;
         });
+        lines.push(lines.length === 0 ? currentLine : currentLine.trimStart());
 
-        // Print final summary
-        if (minifiedJSdata.length > 0) {
-            printIfNotQuiet(`\n${bg}Minification complete!${nc}`);
-            printIfNotQuiet(
-                `${ minifiedJSdata.length } file${ minifiedJSdata.length > 1 ? 's' : '' } minified.`);
-        } else printIfNotQuiet(`${by}No unminified JavaScript files processed successfully.${nc}`);
-        const failedCnt = unminnedJSfiles.length - minifiedJSdata.length;
-        if (failedCnt > 0) {
-            printIfNotQuiet(`\n${br + failedCnt} file${ failedCnt > 1 ? 's' : '' } failed to minify:${nc}`);
-            printIfNotQuiet(failedJSpaths.join(', '));
-        }
+        // Print formatted msg
+        lines.forEach((line, index) => console.info(
+            index === 0 ? line // print 1st line unindented
+                : ' '.repeat(indentation) + line // print subsequent lines indented
+        ));
     }
+
+    function printIfNotQuiet(msg) { if (!config.quietMode) console.info(msg); }
 }
-
-// Define LOGGING functions
-
-function printHelp(msg) { // wrap msg + indent 2nd+ lines (for --help screen)
-    const terminalWidth = process.stdout.columns || 80,
-          indentation = 29, lines = [], words = msg.match(/\S+|\s+/g);
-
-    // Split msg into lines of appropriate lengths
-    let currentLine = '';
-    words.forEach(word => {
-        const lineLength = terminalWidth - ( lines.length === 0 ? 0 : indentation );
-        if (currentLine.length + word.length > lineLength) { // cap/store it
-            lines.push(lines.length === 0 ? currentLine : currentLine.trimStart());
-            currentLine = '';
-        }
-        currentLine += word;
-    });
-    lines.push(lines.length === 0 ? currentLine : currentLine.trimStart());
-
-    // Print formatted msg
-    lines.forEach((line, index) => console.info(
-        index === 0 ? line // print 1st line unindented
-            : ' '.repeat(indentation) + line // print subsequent lines indented
-    ));
-}
-
-function printIfNotQuiet(msg) { if (!config.quietMode) console.info(msg); }
