ecd7a34c52ba387b517604f3756228723a134480
adamlui@protonmail.com <adamlui@protonmail.com>
2024-03-17 12:28:53 -0700

    Added generate-pw files

---
+++ generate-pw/README.md
@@ -0,0 +1,54 @@
+# > generate-pw
+
+### Randomly generate cryptographically-secure passwords.
+
+<a href="https://github.com/adamlui/js-utils/tree/main/generate-pw"><img height=28 src="https://img.shields.io/badge/Latest_Build-1.0.0-fc7811.svg?logo=icinga&logoColor=white&labelColor=464646&style=for-the-badge"></a>
+
+## ⚡ Installation
+
+As a **global utility**:
+
+```
+npm install -g generate-pw
+```
+
+As a **dev dependency**, from your project root:
+
+```
+npm install -D generate-pw
+```
+
+As a **runtime dependency**, from your project root:
+
+```
+npm install generate-pw
+```
+
+## 💻 Command line usage
+
+The basic **global command** is:
+
+```
+generate-pw
+```
+
+**💡 Note:** Pass `--qty=n` where `n` is the number of passwords to generate for multiple results.
+<br><br>
+
+## 🏛️ MIT License
+
+**Copyright © 2024 [Adam Lui](https://github.com/adamlui)**
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+<br>
+
+<img height=6px width="100%" src="https://raw.githubusercontent.com/adamlui/js-utils/main/docs/images/aqua-separator.png">
+
+<a href="https://github.com/adamlui/js-utils">**Home**</a> /
+<a href="https://github.com/adamlui/js-utils/discussions">Discuss</a> /
+<a href="#-generate-pw">Back to top ↑</a>
---
+++ generate-pw/generate-pw.js
@@ -0,0 +1,146 @@
+#!/usr/bin/env node
+
+// Import crypto.randomInt() for secure RNG
+const { randomInt } = require('crypto');
+
+// Init character sets
+const charsets = {
+    lower: 'abcdefghijklmnopqrstuvwxyz',
+    upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numbers: '0123456789',
+    symbols: '!@#$%^&*()-_=+[]{}/\\|;:\'",.<>?'
+};
+
+// Define MAIN functions
+
+function generatePassword(options = {}) { 
+
+    // Init options
+    const defaultOptions = {
+        length: 8,       // length of password
+        qty: 1,          // number of passwords to generate
+        charset: '',     // characters to include
+        exclude: '',     // characters to exclude
+        numbers: false,  // include numberChars
+        symbols: false,  // include symbolChars
+        lowercase: true, // include lowercase letters
+        uppercase: true, // include uppercase letters
+        strict: false    // require at least one char from each enabled set
+    };
+    options = { ...defaultOptions, ...options };
+
+    if (options.qty > 1) { // return array of [qty] password strings
+        const { qty, ...otherOptions } = options;
+        return generatePasswords(qty, otherOptions);
+
+    } else { // return single password string
+
+        // Init password's char set
+        let pwCharset = options.charset || ( // use passed [charset], or construct from options
+            (options.numbers ? charsets.numbers : '')
+              + (options.symbols ? charsets.symbols : '')
+              + (options.lowercase ? charsets.lower : '') 
+              + (options.uppercase ? charsets.upper : '')
+        );
+        if (pwCharset === '') // all flags false + no charset passed
+            pwCharset = charsets.lower + charsets.upper; // default to upper + lower
+
+        // Exclude passed `exclude` chars
+        pwCharset = pwCharset.replace(new RegExp(`[${ options.exclude }]`, 'g'), '');
+
+        // Generate unstrict password
+        let password = '';
+        for (let i = 0; i < options.length; i++) {
+            const randomIndex = randomInt(0, pwCharset.length);
+            password += pwCharset.charAt(randomIndex);
+        }
+
+        // Enforce strict mode if enabled
+        if (options.strict) { 
+            const charTypes = ['number', 'symbol', 'lower', 'upper'],
+                  requiredCharTypes = charTypes.filter(charType => options[charType + 's'] || options[charType + 'case']);
+
+            // Init flags
+            for (const charType of requiredCharTypes)
+                global['has' + charType.charAt(0).toUpperCase() + charType.slice(1)] = false;
+            for (let i = 0; i < password.length; i++)
+                for (const charType of requiredCharTypes)
+                    if ((charsets[charType] || charsets[charType + 's']).includes(password.charAt(i)))
+                        global['has' + charType.charAt(0).toUpperCase() + charType.slice(1)] = true;
+
+            // Modify password if necessary
+            const maxReplacements = Math.min(password.length, requiredCharTypes.length),
+                  replacedPositions = []; let replacementCnt = 0;
+            for (const charType of requiredCharTypes) {
+                if (replacementCnt < maxReplacements) {
+                    if (!global['has' + charType.charAt(0).toUpperCase() + charType.slice(1)]) {
+                        let replacementPos;
+                        do replacementPos = randomInt(0, password.length); // pick random pos
+                        while (replacedPositions.includes(replacementPos)); // check if pos already replaced
+                        replacedPositions.push(replacementPos); // track new replacement pos
+                        const replacementCharSet = charsets[charType] || charsets[charType + 's'];
+                        password = password.substring(0, replacementPos) // perform actual replacement
+                                 + replacementCharSet.charAt(randomInt(0, replacementCharSet.length))
+                                 + password.substring(replacementPos + 1);
+                        replacementCnt++;
+            }}}
+        }
+
+        return password;
+    }
+}
+
+function generatePasswords(qty, options) {
+    qty = parseInt(qty);
+    if (isNaN(qty)) return console.error(
+        'ERROR: First argument <qty> of generatePasswords() must be an integer');
+    const passwords = [];
+    for (let i = 0; i < qty; i++) passwords.push(generatePassword(options));
+    return passwords;
+}
+
+// Export function if script was required
+if (require.main !== module) module.exports = { generatePassword, generatePasswords };
+
+else { // run as CLI tool
+
+    // Init UI colors
+    const nc = '\x1b[0m',    // no color
+          br = '\x1b[1;91m', // bright red
+          bw = '\x1b[1;97m'; // bright white
+
+    // Load settings from args
+    const config = { 
+        length: parseInt(process.argv.find(arg => /^--?length/.test(arg))?.split('=')[1]) || 8,
+        qty: parseInt(process.argv.find(arg => /^--?qu?a?n?ti?t?y=\d+$/.test(arg))?.split('=')[1]) || 1,
+        charset: process.argv.find(arg => /^--?chars/.test(arg))?.split('=')[1] || charsets.lower + charsets.upper,
+        excludeChars: process.argv.find(arg => /^--?exclude/.test(arg))?.split('=')[1] || '',
+        includeNums: process.argv.some(arg => /^--?(?:n|include-?numbers?|numbers?=(?:true|1))$/.test(arg)),
+        includeSymbols: process.argv.some(arg => /^--?(?:s|include-?symbols?|symbols?=(?:true|1))$/.test(arg)),
+        excludeLowerChars: process.argv.some(arg =>
+            /^--?(?:L|exclude-?lower-?(?:case)?|lower-?(?:case)?=(?:false|0))$/.test(arg)),
+        excludeUpperChars: process.argv.some(arg =>
+            /^--?(?:L|exclude-?upper-?(?:case)?|upper-?(?:case)?=(?:false|0))$/.test(arg)),
+        strictMode: process.argv.some(arg => /^--?s(?:trict)?/.test(arg))
+    };
+
+    // Show VERSION number if -v or --version passed
+    if (process.argv.some(arg => /^--?ve?r?s?i?o?n?$/.test(arg))) {
+        console.info('v' + require('./package.json').version);
+
+    } else { // run MAIN routine
+        for (const numArgType of ['length', 'qty']) {
+            if (config['numArgType'] < 1) {
+                console.error(`\n${br}Error: '${ numArgType }' argument must be 1 or greater.${nc}`);
+                process.exit(1);
+        }}
+        const funcOptions = {
+            length: config.length, qty: config.qty, charset: config.charset, exclude: config.excludeChars,
+            numbers: config.includeNums, symbols: config.includeSymbols,
+            lowercase: !config.excludeLowerChars, uppercase: !config.excludeUpperChars,
+            strict: config.strictMode
+        };
+        const pwGenResult = generatePassword(funcOptions);
+        console.log('\n' + bw + ( Array.isArray(pwResult) ? pwResult.join('\n') : pwResult ) + nc);
+    }
+}
