8e17cc054e3e6ed12a687514f6029c8ab51949e2
adamlui@protonmail.com <adamlui@protonmail.com>
2024-03-26 00:29:03 -0700

    Condensed final log routine in API search functions

--- scss-to-css/scss-to-css.js
+++ scss-to-css/scss-to-css.js
@@ -48,13 +48,11 @@ function findSCSS(searchDir, options = {}) {
     });
 
     // Log/return final result
-    if (!options.isRecursing && options.verbose) {
-        console.info('findSCSS() » Search complete! '
-            + ( scssFiles.length === 0 ? 'No' : scssFiles.length )
-                + ` file${ scssFiles.length > 1 ? 's' : '' } found.`
-            + ( findSCSS.caller.name !== 'compile' && require.main !== module ?
-                '\nfindSCSS() » Check returned array.' : '' ));
-    }
+    if (!options.isRecursing && options.verbose) console.info(
+        'findSCSS() » Search complete! ' + ( scssFiles.length === 0 ? 'No' : scssFiles.length )
+            + ` file${ scssFiles.length > 1 ? 's' : '' } found.`
+        + ( findSCSS.caller.name !== 'compile' && require.main !== module ?
+            '\nfindSCSS() » Check returned array.' : '' ));
     return options.isRecursing || scssFiles.length > 0 ? scssFiles : [];
 }
 
