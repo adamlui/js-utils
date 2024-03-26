f54236da4da4aa9de18c32b94825517043c6fbba
adamlui@protonmail.com <adamlui@protonmail.com>
2024-03-26 00:22:14 -0700

    Removed unnecessary missing `searchDir` validation from `findSCSS()`

--- scss-to-css/scss-to-css.js
+++ scss-to-css/scss-to-css.js
@@ -14,8 +14,6 @@ function findSCSS(searchDir, options = {}) {
     options = { ...defaultOptions, ...options };
 
     // Validate searchDir
-    if (!searchDir) return console.error(
-        'findSCSS() » ERROR: Please supply a `searchDir` as 1st arg.');
     else if (typeof searchDir !== 'string') return console.error(
         'findSCSS() » ERROR: 1st arg <searchDir> must be a string.');
     else { // verify searchDir path existence
