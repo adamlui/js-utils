fcd4c1f3b9ca63cbd53ebc55d7d6d482b67bb327
adamlui@protonmail.com <adamlui@protonmail.com>
2024-03-30 20:21:36 -0700

    Moved latest minified release comment last in header comments

--- generate-ip/src/generate-ip.js
+++ generate-ip/src/generate-ip.js
@@ -1,7 +1,7 @@
 // © 2024 Adam Lui & contributors under the MIT license.
 // Source: https://github.com/adamlui/js-utils/tree/main/generate-ip
-// Latest minified release: https://cdn.jsdelivr.net/npm/generate-ip/dist/generate-ip.min.js
 // Documentation: https://github.com/adamlui/js-utils/tree/main/generate-ip#readme
+// Latest minified release: https://cdn.jsdelivr.net/npm/generate-ip/dist/generate-ip.min.js
 
 // IMPORT secure crypto RNG
 let randomInt;
