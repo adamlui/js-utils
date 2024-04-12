a0633d5d5950d8f477a2ebe1f447db67047f04bf
adamlui@protonmail.com <adamlui@protonmail.com>
2024-04-12 07:00:26 -0700

    Added header comment insertion into build script

--- minify.js/node.js/utils/build.sh
+++ minify.js/node.js/utils/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-node src/cli src dist
+node src/cli src dist --comment=" \
+© 2023–2024 Adam Lui & contributors\n \
+Source: https://github.minify-js.org/tree/main/node.js/src\n \
+Documentation: https://docs.minify-js.org"
