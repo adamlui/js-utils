d3b0bb2ed4d55d14b53e4799f581cd46ea7a7512
adamlui@protonmail.com <adamlui@protonmail.com>
2024-04-13 00:45:03 -0700

    Added MIT license to copyright notice

--- minify.js/node.js/utils/build.sh
+++ minify.js/node.js/utils/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 node src/cli src dist --comment=" \
-© 2023–2024 Adam Lui & contributors\n \
+© 2023–2024 Adam Lui & contributors under the MIT license.\n \
 Source: https://github.minify-js.org/tree/main/node.js/src\n \
 Documentation: https://docs.minify-js.org"
