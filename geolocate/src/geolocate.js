6a891f80baf752bcecf0acd51fda120bcbc83895
adamlui@protonmail.com <adamlui@protonmail.com>
2024-04-13 10:45:09 -0700

    Added `verbose` option + `validateOptions()`

--- geolocate/src/geolocate.js
+++ geolocate/src/geolocate.js
@@ -5,14 +5,16 @@
 
 // Define API functions
 
-async function geolocate(ip) {
+async function geolocate(ip, options = {}) {
 
-    const docURL = 'https://github.com/adamlui/js-utils/tree/main/geolocate#locateip';
+    const docURL = 'https://github.com/adamlui/js-utils/tree/main/geolocate#locateip',
+          exampleCall = 'geolocate(\'8.8.8.8\', { verbose: false })',
+          defaultOptions = { verbose: true /* enable logging */ };
 
     // Init/validate IP
     ip = ip || await getOwnIP();
     let ipIsValid;
-    try { // to use Node.js module
+    try { // to use Node.js generate-ip for validation
         ipIsValid = require('generate-ip').ipv4.validate;
     } catch (err) { // use jsDelivr's latest copy of generate-ip
         await import('https://cdn.jsdelivr.net/npm/generate-ip/dist/generate-ip.min.js');
@@ -23,6 +25,10 @@ async function geolocate(ip) {
         console.info('geolocate() » For more help, please visit ' + docURL);
         return;
     }
+
+    // Validate/init options
+    if (!validateOptions(options, defaultOptions, docURL, exampleCall)) return;
+    options = { ...defaultOptions, ...options }; // merge validated options w/ missing default ones
 
     try { // to fetch/get/return geolocation data
         let response;
@@ -47,6 +53,47 @@ async function geolocate(ip) {
             } catch (err) { console.error('geolocate() »', err); }
         }));
     }
+}
+
+// Define INTERNAL validation function
+
+function validateOptions(options, defaultOptions, docURL, exampleCall) {
+
+    // Init option strings/types
+    const strDefaultOptions = JSON.stringify(defaultOptions, null, 2)
+        .replace(/"([^"]+)":/g, '$1:') // strip quotes from keys
+        .replace(/"/g, '\'') // replace double quotes w/ single quotes
+        .replace(/\n\s*/g, ' '); // condense to single line
+    const strValidOptions = Object.keys(defaultOptions).join(', '),
+          booleanOptions = Object.keys(defaultOptions).filter(key => typeof defaultOptions[key] == 'boolean');
+
+    // Define print functions
+    const logPrefix = ( validateOptions.caller?.name || 'validateOptions' ) + '() » ';
+    const printValidOptions = () => {
+        console.info(`${ logPrefix }Valid options: [ ${ strValidOptions } ]`);
+        console.info(`${ logPrefix }If omitted, default settings are: ${ strDefaultOptions }`);
+    };
+    const printDocURL = () => {
+        console.info(`${ logPrefix }For more help, please visit ${docURL}`); };
+
+    // Validate options
+    if (typeof options != 'object') { // validate as obj
+        console.error(`${ logPrefix }ERROR: [options] can only be an object of key/values.`);
+        console.info(`${ logPrefix }Example valid call: ${ exampleCall }`);
+        printValidOptions(); printDocURL(); return false;
+    }
+    for (const key in options) { // validate each key
+        if (!Object.prototype.hasOwnProperty.call(defaultOptions, key)) {
+            console.error(
+                `${ logPrefix }ERROR: \`${ key }\` is an invalid option.`);
+            printValidOptions(); printDocURL(); return false;
+        } else if (booleanOptions.includes(key) && typeof options[key] != 'boolean') {
+            console.error(
+                `${ logPrefix }ERROR: [${ key }] option can only be \`true\` or \`false\`.`);
+            printDocURL(); return false;
+        }
+    }
+    return true;
 }
 
 // EXPORT API functions
