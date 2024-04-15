8a9c6bd801db678d855bd77399e73f19b9188d31
adamlui <adamlui@users.noreply.github.com>
2024-04-15 04:04:55 +0000

    Added platforms to doc URLs ↞ [auto-sync from `adamlui/minify.js`]

--- minify.js/gulp/utils/build.sh
+++ minify.js/gulp/utils/build.sh
@@ -3,4 +3,4 @@
 minify-js src dist --comment=" \
 © 2024 Adam Lui under the MIT license.\n \
 Source: https://code.minify-js.org/gulp\n \
-Documentation: https://docs.minify-js.org"
+Documentation: https://docs.minify-js.org/gulp"
