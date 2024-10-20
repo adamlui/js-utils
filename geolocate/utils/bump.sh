5a87b2d618ccf4e6fc151bdfef4b78a715ccfea2
adamlui@protonmail.com <adamlui@protonmail.com>
2024-10-20 13:06:10 -0700

    Uppercased var names for readability

--- geolocate/utils/bump.sh
+++ geolocate/utils/bump.sh
@@ -6,16 +6,16 @@
 # >>> commit build to Git >>> push all changes to GitHub >>> publish to npm (optional)
 
 # Init UI colors
-nc="\033[0m"    # no color
-br="\033[1;91m" # bright red
-by="\033[1;33m" # bright yellow
-bg="\033[1;92m" # bright green
-bw="\033[1;97m" # bright white
+NC="\033[0m"    # no color
+BR="\033[1;91m" # bright red
+BY="\033[1;33m" # bright yellow
+BG="\033[1;92m" # bright green
+BW="\033[1;97m" # bright white
 
 # Validate version arg
-ver_types=("major" "minor" "patch")
-if [[ ! "${ver_types[@]}" =~ "$1" ]] ; then
-    echo "${br}Invalid version argument. Please specify 'major', 'minor', or 'patch'.${nc}"
+VER_TYPES=("major" "minor" "patch")
+if [[ ! "${VER_TYPES[@]}" =~ "$1" ]] ; then
+    echo "${BR}Invalid version argument. Please specify 'major', 'minor', or 'patch'.${NC}"
     exit 1 ; fi
 
 # Determine new version to bump to
@@ -29,11 +29,11 @@ esac
 new_ver=$(printf "%s.%s.%s" "${subvers[@]}")
 
 # Bump version in package.json + package-lock.json
-echo -e "${by}Bumping versions in package manifests...${bw}"
+echo -e "${BY}Bumping versions in package manifests...${BW}"
 npm version --no-git-tag-version "$new_ver"
 
 # Bump versions in READMEs
-echo -e "${by}\nBumping versions in READMEs...${bw}"
+echo -e "${BY}\nBumping versions in READMEs...${BW}"
 pkg_name=$(node -pe "require('./package.json').name" | sed -e 's/^@[a-zA-Z0-9-]*\///' -e 's/^@//')
 sed_actions=(
     # Latest Build shield link
@@ -49,41 +49,41 @@ find . -name 'README.md' "${sed_actions[@]}"
 echo "v$new_ver"
 
 # Commit bumps to Git
-echo -e "${by}\nCommitting bumps to Git...\n${nc}"
+echo -e "${BY}\nCommitting bumps to Git...\n${NC}"
 find . -name "README.md" -exec git add {} +
 git add package*.json
 git commit -n -m "Bumped $pkg_name versions to $new_ver"
 
 # Build minified JS to dist/
-echo -e "${by}\nBuilding minified JS...\n${nc}"
+echo -e "${BY}\nBuilding minified JS...\n${NC}"
 bash utils/build.sh
 
 # Update jsDelivr URLs for GitHub assets w/ commit hash
-echo -e "${by}\nUpdating jsDelivr URLs for GitHub assets w/ commit hash...${nc}"
-bump_hash=$(git rev-parse HEAD)
+echo -e "${BY}\nUpdating jsDelivr URLs for GitHub assets w/ commit hash...${NC}"
+BUMP_HASH=$(git rev-parse HEAD)
 old_file=$(<dist/cli.min.js)
-sed -i -E "s|(cdn\.jsdelivr\.net\/gh\/[^/]+\/[^@/\"']+)[^/\"']*|\1@$bump_hash|g" dist/cli.min.js
+sed -i -E "s|(cdn\.jsdelivr\.net\/gh\/[^/]+\/[^@/\"']+)[^/\"']*|\1@$BUMP_HASH|g" dist/cli.min.js
 new_file=$(<dist/cli.min.js)
 if [[ "$old_file" != "$new_file" ]]
-    then echo -e "${bw}$bump_hash${nc}"
+    then echo -e "${BW}$BUMP_HASH${NC}"
     else echo "No jsDelivr URLs for GH assets found in built files"
 fi
 
 # Commit build to Git
-echo -e "${by}\nCommitting build to Git...\n${nc}"
+echo -e "${BY}\nCommitting build to Git...\n${NC}"
 git add ./dist/*.js
 git commit -n -m "Built $pkg_name v$new_ver"
 
 # Push all changes to GiHub
-echo -e "${by}\nPushing to GitHub...\n${nc}"
+echo -e "${BY}\nPushing to GitHub...\n${NC}"
 git push
 
 # Publish to NPM
 if [[ "$*" == *"--publish"* ]] ; then
-    echo -e "${by}\nPublishing to npm...\n${nc}"
+    echo -e "${BY}\nPublishing to npm...\n${NC}"
     npm publish ; fi
 
 # Print final summary
-echo -e "\n${bg}Successfully bumped to v$new_ver$(
+echo -e "\n${BG}Successfully bumped to v$new_ver$(
     [[ "$*" == *"--publish"* ]] && echo ' and published to npm' || echo ''
-)!${nc}"
+)!${NC}"
