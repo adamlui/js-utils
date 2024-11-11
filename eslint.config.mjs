d7dd185b60dda3f3544bf65d8399e3fc4585c695
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-11 12:01:30 -0800

    Moved `...markdown.configs.recommended` into MD config for readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,6 @@ import markdown from '@eslint/markdown'
 import eslintPluginYml from 'eslint-plugin-yml'
 
 export default [
-    ...markdown.configs.recommended,
     { ignores: ['**/*.min.js', '**/sandbox/*'] },
     {
         files: ['**/*.js', '**/*.mjs'], ...js.configs.recommended,
@@ -28,8 +27,9 @@ export default [
     { files: ['**/*.mjs'], languageOptions: { sourceType: 'module' }},
     { files: ['**/*.json'], ignores: ['**/package-lock.json'], language: 'json/json', ...json.configs.recommended },
     {
-        files: ['**/*.md'],
+        files: ['**/*.md'], language: 'markdown/commonmark', plugins: { markdown },
         rules: {
+            ...markdown.configs.recommended[0].rules,
             'markdown/heading-increment': 'off', // allow headings to skip levels
             'markdown/fenced-code-language': 'off' // allow code blocks w/ no language specified
         }
