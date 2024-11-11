8143f85abc18535a796f0b2ca8b87e6921821026
Adam Lui 刘展鹏 <adam@kudoai.com>
2024-11-11 05:31:48 -0800

    Removed semicolon enforcement

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,6 @@ export default [
     {
         rules: {
             'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
-            'semi': ['error', 'always'], // enforce semicolons at end of statement
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
             'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
