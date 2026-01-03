df9599239cc81c64fc471ecc435d575e59fabdf7
Adam Lui <adam@kudoai.com>
2026-01-03 04:22:40 -0800

    Added optional chaining to `findSCSS.caller.name`

--- scss-to-css/node.js/src/scss-to-css.js
+++ scss-to-css/node.js/src/scss-to-css.js
@@ -62,7 +62,7 @@ function findSCSS(searchDir, options = {}) {
         console.info('findSCSS() » Search complete! '
             + ( scssFiles.length == 0 ? 'No' : scssFiles.length )
             + ` file${ scssFiles.length == 1 ? '' : 's' } found.`)
-        if (findSCSS.caller.name != 'compile' && typeof window != 'undefined')
+        if (findSCSS.caller?.name != 'compile' && typeof window != 'undefined')
             console.info('findSCSS() » Check returned array.')
     }
     return options.isRecursing || scssFiles.length ? scssFiles : []
