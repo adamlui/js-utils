c2149d232fa69d09bbda28a70b3e5a24f0c7aba7
Adam Lui <adam@kudoai.com>
2026-01-23 08:26:37 -0800

    Moved language routines to language.js

--- minify.js/node.js/src/cli/index.js
+++ minify.js/node.js/src/cli/index.js
@@ -7,10 +7,10 @@
 
     // Import LIBS
     const clipboardy = require('node-clipboardy'),
-          data = require(`./lib/data${ env.devMode ? '' : '.min' }.js`),
         { execSync } = require('child_process'),
           fs = require('fs'),
-          minifyJS = require(`./minify${ env.devMode ? '' : '.min' }.js`),
+          language = require(`./lib/language${ env.devMode ? '' : '.min' }.js`),
+          minifyJS = require(`../minify${ env.devMode ? '' : '.min' }.js`),
           path = require('path')
 
     // Init APP data
@@ -47,35 +47,8 @@
           bg = '\x1b[1;92m', // bright green
           bw = '\x1b[1;97m'  // bright white
 
-    // Init sys LANGUAGE
-    if (process.platform == 'win32')
-        try {
-            env.sysLang = execSync(
-                '(Get-Culture).TwoLetterISOLanguageName', { shell: 'powershell', encoding: 'utf-8' }
-            ).trim()
-        } catch (err) { console.error('ERROR loading system language:', err.message) }
-    else { // macOS/Linux
-        const pe = process.env
-        env.sysLang = (pe.LANG || pe.LANGUAGE || pe.LC_ALL || pe.LC_MESSAGES || pe.LC_NAME || 'en')?.split('.')[0]
-    }
-
-    // Load MESSAGES
-    app.msgs = data.flatten(
-        require(`${ env.devMode ? '../../_locales/en' : './data' }/messages.json`), { type: 'message' })
-    if (!env.sysLang.startsWith('en'))
-        try { // to fetch from jsDelivr
-            const msgHostDir = `${app.urls.jsdelivr}@${app.commitHashes.locales}/_locales/`,
-                  msgLocaleDir = `${ env.sysLang ? env.sysLang.replace('-', '_') : 'en' }/`
-            let msgHref = `${msgHostDir}${msgLocaleDir}messages.json`, msgFetchTries = 0
-            while (msgFetchTries < 3)
-                try { app.msgs = data.flatten(await (await data.fetch(msgHref)).json(), { type: 'message' }) ; break }
-                catch (err) { // if bad response
-                    msgFetchTries++ ; if (msgFetchTries == 3) break // try original/region-stripped/EN only
-                    msgHref = env.sysLang.includes('-') && msgFetchTries == 1 ? // if regional lang on 1st try...
-                        msgHref.replace(/([^_]*)_[^/]*(\/.*)/, '$1$2') // ...strip region before retrying
-                            : `${msgHostDir}en/messages.json` // else use default English messages
-                }
-        } catch (err) { console.error('ERROR loading messages:', err.message) }
+    // Init LANGUAGE props
+    language.initSysLang() ; await language.initMsgs()
 
     // Load SETTINGS from args
     process.argv.forEach(arg => {
