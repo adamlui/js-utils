055da074682d6f60ffc194485dd6bcc447d0d371
Adam Lui <adam@kudoai.com>
2026-01-31 23:28:56 -0800

    Improved comment

--- generate-ip/src/cli/lib/data.js
+++ generate-ip/src/cli/lib/data.js
@@ -1,6 +1,6 @@
 module.exports = {
     fetch(url) { // to support Node.js < v21
-        return typeof fetch == 'undefined' ? new Promise((resolve, reject) => { // using https? pkg
+        return typeof fetch == 'undefined' ? new Promise((resolve, reject) => { // using https?.get()
             const protocol = url.match(/^([^:]+):\/\//)[1]
             if (!/^https?$/.test(protocol)) reject(new Error(`${app.msgs.error_invalidURL}.`))
             require(protocol).get(url, resp => {
