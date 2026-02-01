bcc81be8295aae81ecdc328af2544c3fb34f0c42
Adam Lui <adam@kudoai.com>
2026-01-31 18:59:36 -0800

    Localized msgs in `log.helpCmdAndDocURL()`

--- geolocate/src/cli/lib/log.js
+++ geolocate/src/cli/lib/log.js
@@ -98,8 +98,9 @@ module.exports = {
     },
 
     helpCmdAndDocURL() {
-        console.info(
-            `\nFor more help, type 'scss-to-css --help' or visit\n${
+        console.info(`\n${
+            app.msgs.info_moreHelp}, ${app.msgs.info_type} ${app.name} --help' ${
+                app.msgs.info_or} ${app.msgs.info_visit}\n${
                 this.colors.bw}${app.urls.docs}${this.colors.nc}`
         )
     },
