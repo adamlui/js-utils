9d2ebf523c0ff8bd769db701b812da0f1a5f7b26
Adam Lui <adam@kudoai.com>
2026-02-05 12:58:57 -0800

    Built minify.js v2.2.1

--- minify.js/node.js/dist/data/minify.config.mjs
+++ minify.js/node.js/dist/data/minify.config.mjs
@@ -9,16 +9,22 @@
  */
 
 export default {
+
+    // Info options
     dryRun: false,            // don't actually minify the file(s), just show if they will be processed
+    quietMode: false,         // suppress all logging except errors
+
+    // Input options
     includeDotFolders: false, // include dotfolders in file search
     includeDotFiles: false,   // include dotfiles in file search
     noRecursion: false,       // disable recursive file searching
+    ignores: '',              // files/dirs to exclude from minification
+
+    // Output options
     noMangle: false,          // disable mangling names
     noFilenameChange: false,  // disable changing file extension to .min.js
     rewriteImports: false,    // update import paths from .js to .min.js
     copy: false,              // copy minified code to clipboard instead of write to file if single file processed
     relativeOutput: false,    // output files relative to each src file instead of to input root
-    quietMode: false,         // suppress all logging except errors
-    ignores: '',              // files/dirs to exclude from minification
     comment: ''               // header comment to prepend to minified code
 }
