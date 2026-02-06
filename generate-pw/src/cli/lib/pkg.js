3f342962035cbc04aa1136dcbb7f328501f3496a
Adam Lui <adam@kudoai.com>
2026-02-05 19:42:28 -0800

    Reversed conditions in `pkg.getVer()` for faster return on `type = 'any'

--- generate-pw/src/cli/lib/pkg.js
+++ generate-pw/src/cli/lib/pkg.js
@@ -3,18 +3,18 @@ const log = require(`./log${ env.devMode ? '' : '.min' }.js`)
 module.exports = {
     getVer(type = 'any') { // or <'global'|'local'>
         let pkgVer
-        if (['any', 'global'].includes(type))
+        if (type != 'global')
+            try { // get local ver
+                const localManifestPath = require('path').resolve(
+                    process.cwd(), 'node_modules', cli.name, 'package.json')
+                pkgVer = require(localManifestPath).version
+            } catch (err) { log.debug(`${cli.msgs.error_readingLocalPkgVer}: ${err.message}`) }
+        if (type == 'global' || type == 'all' && !pkgVer)
             try { // get global ver
                 pkgVer = require('child_process').execSync(
                     `npm view ${JSON.stringify(cli.name)} version`
                 ).toString().trim()
             } catch (err) { log.debug(`${cli.msgs.error_failedToFetchGlobalVer}: ${err.message}`) }
-        if (type == 'any' && !pkgVer || type == 'local')
-            try { // get local ver
-                const localManifestPath =require('path').resolve(
-                    process.cwd(), 'node_modules', cli.name, 'package.json')
-                pkgVer = require(localManifestPath).version
-            } catch (err) { log.debug(`${cli.msgs.error_readingLocalPkgVer} ${err.message}`) }
         return pkgVer
     }
 }
