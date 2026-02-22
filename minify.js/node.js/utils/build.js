8a9eedf7e5b6e5ccf8e35b04cffac969a96a561d
Adam Lui <adam@kudoai.com>
2026-02-22 09:48:21 -0800

    Fixed `copyfiles` to create dest dirs, added copy/success logs, extracted `execOptions`

--- minify.js/node.js/utils/build.js
+++ minify.js/node.js/utils/build.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// Copies data + minifies JS to dist/
+// Copies data/JS + minifies API to dist/
 
 // NOTE: Pass --<js|minify> to minify JS only
 // NOTE: Pass --data to copy data only
@@ -8,7 +8,8 @@
 
 const args = process.argv.slice(2),
     { execSync } = require('child_process'),
-      fs = require('fs')
+      fs = require('fs'),
+     _log = require('../src/cli/lib/log')
 
 globalThis.cli = require('../package-data.json')
 cli.config = {
@@ -16,6 +17,7 @@ cli.config = {
     jsOnly: args.some(arg => /^--?(?:js|minify)$/.test(arg)),
     jsonOnly: args.some(arg => /^--?json$/.test(arg))
 }
+cli.version = require('../package.json').version
 
 // Copy data
 if (!cli.config.jsOnly) {
@@ -24,18 +26,26 @@ if (!cli.config.jsOnly) {
     if (fs.existsSync(dataOutDir)) // clear existing data
         fs.rmSync(dataOutDir, { recursive: true })
     fs.mkdirSync(dataOutDir, { recursive: true })
-    fs.copyFileSync(`../_locales/en/${filenames.msgs}`, `${dataOutDir}/${filenames.msgs}`)
-    fs.copyFileSync(filenames.pkgData, `${dataOutDir}/${filenames.pkgData}`)
-    if (!cli.config.jsonOnly) fs.copyFileSync(filenames.config, `${dataOutDir}/${filenames.config}`)
+    verboseCopy(`../_locales/en/${filenames.msgs}`, `${dataOutDir}/${filenames.msgs}`)
+    verboseCopy(filenames.pkgData, `${dataOutDir}/${filenames.pkgData}`)
+    if (!cli.config.jsonOnly) verboseCopy(filenames.config, `${dataOutDir}/${filenames.config}`)
 }
 
 // Copy/minify JS
 if (!cli.config.dataOnly) {
     cli.headerComment = `© ${cli.copyrightYear} ${cli.author} under the ${cli.license} license.\\n`
                       + `Source: ${cli.urls.src}\\nDocumentation: ${cli.urls.docs}`
+    const execOptions = { stdio: 'inherit' }
     if (fs.existsSync('dist'))
         for (const item of fs.readdirSync('dist', { recursive: true }))
             if (!/data(?:[/\\]|$)/.test(item)) fs.rmSync(`dist/${item}`, { recursive: true, force: true })
-    execSync(`node ../node.js/src/cli src dist --ignores=cli --comment="${cli.headerComment}"`, { stdio: 'inherit' })
-    execSync(`npx copyfiles -e "**/*cache*/**" "src/cli/**/*" dist`, { stdio: 'inherit' })
+    execSync(`node src/cli src dist --ignores=cli --comment="${cli.headerComment}"`, execOptions)
+    execSync(`npx copyfiles -V -e "**/*cache*/**" -u 1 "src/cli/**" dist`, execOptions)
+}
+
+_log.success(`${cli.name} v${cli.version} build complete!`)
+
+function verboseCopy(src, dest) {
+    console.info(`Copying ${_log.colors.bo}${src}${_log.colors.nc} to ${_log.colors.by}${dest}${_log.colors.nc}...`)
+    fs.copyFileSync(src, dest)
 }
