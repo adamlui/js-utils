9c5ecba5333770bdcc2e03193d6c8ee5a1efda11
Adam Lui <adam@kudoai.com>
2026-02-22 10:21:16 -0800

    Moved `args` + `cli.config` to `script`, renamed `cli` to `pkg`

--- scss-to-css/utils/build.js
+++ scss-to-css/utils/build.js
@@ -6,21 +6,22 @@
 // NOTE: Pass --data to copy data only
 // NOTE: Pass --json to copy JSON only
 
-const args = process.argv.slice(2),
-    { execSync } = require('child_process'),
-      fs = require('fs'),
-     _log = require('../src/cli/lib/log')
+const { execSync } = require('child_process'),
+        fs = require('fs'),
+       _log = require('../src/cli/lib/log')
 
-globalThis.cli = require('../package-data.json')
-cli.config = {
-    dataOnly: args.some(arg => /^--?data$/.test(arg)),
-    jsOnly: args.some(arg => /^--?(?:js|minify)$/.test(arg)),
-    jsonOnly: args.some(arg => /^--?json$/.test(arg))
+const script = { args: process.argv.slice(2) }
+script.config = {
+    dataOnly: script.args.some(arg => /^--?data$/.test(arg)),
+    jsOnly: script.args.some(arg => /^--?(?:js|minify)$/.test(arg)),
+    jsonOnly: script.args.some(arg => /^--?json$/.test(arg))
 }
-cli.version = require('../package.json').version
+
+const pkg = require('../package-data.json')
+pkg.version = require('../package.json').version
 
 // Copy data
-if (!cli.config.jsOnly) {
+if (!script.config.jsOnly) {
     const dataOutDir = 'dist/data',
           filenames = { pkgData: 'package-data.json', msgs: 'messages.json', config: 'scss-to-css.config.mjs' }
     if (fs.existsSync(dataOutDir)) // clear existing data
@@ -28,22 +29,22 @@ if (!cli.config.jsOnly) {
     fs.mkdirSync(dataOutDir, { recursive: true })
     verboseCopy(`_locales/en/${filenames.msgs}`, `${dataOutDir}/${filenames.msgs}`)
     verboseCopy(filenames.pkgData, `${dataOutDir}/${filenames.pkgData}`)
-    if (!cli.config.jsonOnly) verboseCopy(filenames.config, `${dataOutDir}/${filenames.config}`)
+    if (!script.config.jsonOnly) verboseCopy(filenames.config, `${dataOutDir}/${filenames.config}`)
 }
 
 // Copy/minify JS
-if (!cli.config.dataOnly) {
-    cli.headerComment = `© ${cli.copyrightYear} ${cli.author} under the ${cli.license} license.\\n`
-                      + `Source: ${cli.urls.src}\\nDocumentation: ${cli.urls.docs}`
+if (!script.config.dataOnly) {
+    pkg.headerComment = `© ${pkg.copyrightYear} ${pkg.author} under the ${pkg.license} license.\\n`
+                      + `Source: ${pkg.urls.src}\\nDocumentation: ${pkg.urls.docs}`
     const execOptions = { stdio: 'inherit' }
     if (fs.existsSync('dist'))
         for (const item of fs.readdirSync('dist', { recursive: true }))
             if (!/data(?:[/\\]|$)/.test(item)) fs.rmSync(`dist/${item}`, { recursive: true, force: true })
-    execSync(`npx minify-js src dist --ignores=cli --comment="${cli.headerComment}"`, execOptions)
+    execSync(`npx minify-js src dist --ignores=cli --comment="${pkg.headerComment}"`, execOptions)
     execSync(`npx copyfiles -V -e "**/*cache*/**" -u 1 "src/cli/**" dist`, execOptions)
 }
 
-_log.success(`${cli.name} v${cli.version} build complete!`)
+_log.success(`${pkg.name} v${pkg.version} build complete!`)
 
 function verboseCopy(src, dest) {
     console.info(`Copying ${_log.colors.bo}${src}${_log.colors.nc} to ${_log.colors.by}${dest}${_log.colors.nc}...`)
