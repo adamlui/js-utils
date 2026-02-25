651c06ef16b66657dc979bff47764be4472edc93
Adam Lui <adam@kudoai.com>
2026-02-24 23:12:31 -0800

    Alphabetized functions

--- generate-ip/src/cli/lib/log.js
+++ generate-ip/src/cli/lib/log.js
@@ -7,6 +7,7 @@ const nextMajVer = require('../../../package.json').version.replace(/^(\d+)\..*/
 module.exports = {
     colors,
 
+    break() { console.log() },
     configURL() { this.info(`${cli.msgs.info_exampleValidConfigFile}: ${cli.urls.config}`) },
     configURLandExit(...args) { this.error(...args); this.configURL(); process.exit(1) },
     data(msg) { console.log(`\n${colors.bw}${msg}${colors.nc}`) },
@@ -15,9 +16,8 @@ module.exports = {
     errorAndExit(...args) { this.error(...args); this.helpDocsCmdsDocsURL(); process.exit(1) },
     ifNotQuiet(msg) { if (!cli.config.quietMode) this.info(msg) },
     info(msg) { console.info(`\n${colors.schemes.default[0]}${msg}${colors.nc}`) },
-    break() { console.log() },
-    tip(msg) { console.info(`${colors.by}TIP: ${msg}${colors.nc}`) },
     success(msg) { console.log(`\n${colors.bg}${msg}${colors.nc}`) },
+    tip(msg) { console.info(`${colors.by}TIP: ${msg}${colors.nc}`) },
     warn(...args) { console.warn(`\n${colors.bo}WARNING:`, ...args, colors.nc) },
 
     argDoesNothing(arg) {
