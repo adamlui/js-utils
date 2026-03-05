21cdd2e042c6b0f188647dfec6ffcad87a0a773a
Adam Lui <adam@kudoai.com>
2026-03-05 00:07:05 -0800

    Moved `urls.jsdelivr` to `.jsdelivr.pkg`, added `.jsdelivr.jsutils`

--- scss-to-css/src/cli/lib/jsdelivr.js
+++ scss-to-css/src/cli/lib/jsdelivr.js
@@ -3,7 +3,7 @@ module.exports = {
     getPkgVerURL(version) {
         version ||= cli.version ||= require('./pkg').getVer('local') || 'none'
         const verTag = !/^\d+\.\d+\.\d+$/.test(version) ? 'latest' : `v${version}`
-        return `${cli.urls.jsdelivr}@${verTag}`
+        return `${cli.urls.jsdelivr.pkg}@${verTag}`
     },
 
     getCommitURL(hash = 'latest') { return `${cli.urls.jsdelivr}@${hash}` }
