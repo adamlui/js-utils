79c3bae59854a496c96456a7e6b57cff08c078cb
Adam Lui <adam@kudoai.com>
2026-03-05 05:58:47 -0800

    Added err details to debug msg

--- minify.js/node.js/src/cli/lib/language.js
+++ minify.js/node.js/src/cli/lib/language.js
@@ -65,15 +65,14 @@ module.exports = {
             require(`../../${ env.modes.dev ? '../../_locales/en' : 'data' }/messages.json`))
 
         if (!langCode.startsWith('en')) { // fetch non-English msgs from jsDelivr
-            const nllFilename = 'non-latin-locales.json'
-            try { // ensure terminal supports non-Latin scripts
+            try { // check if terminal supports non-Latin scripts
                 const nonLatinLocales = await (await data.fetch(
-                    `${cli.urls.jsdelivr}@${cli.commitHashes.data}/assets/data/${nllFilename}`
+                    `${cli.urls.jsdelivr}@${cli.commitHashes.data}/assets/data/non-latin-locales.json`
                 )).json()
                 if (nonLatinLocales.includes(langCode.split('-')[0]) && !env.supports.unicode)
                     return msgs // en ones
             } catch (err) {
-                log.debug(`Failed to fetch ${nllFilename}`)
+                log.debug(`Failed to fetch non-Latin locales: ${err}`)
             }
             const msgBaseURL = `${require('./jsdelivr').getCommitURL(cli.commitHashes.locales)}/_locales`
             let msgURL = `${msgBaseURL}/${langCode}/messages.json`, msgFetchesTried = 0
